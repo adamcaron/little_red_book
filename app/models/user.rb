ce68de2bd1225aae508510a0127e5b5a69cc90e6
Adam Caron <adamwcaron@gmail.com>
2016-08-18 11:08:13 -0600

    Made the switch:
    Remove Rails;
    Up and running with Node.
    Run app and see commitment based on time of day.

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  has_secure_password
-
-  validates :firstname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates_confirmation_of :password
-end
