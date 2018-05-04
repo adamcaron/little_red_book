4d669fe435435a592a57f3c90497e719b37169e7
Adam Caron <adamwcaron@gmail.com>
2018-05-03 22:53:46 -0600

    wip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 /* Vow Work */
 const numberOfSessionsPerDay = 6
+<<<<<<< HEAD
 const vows = require('./vows.json')
 
 const moment = require('moment')
@@ -24,3 +25,44 @@ else if ( moment(thisHour).isBetween(15,17,null,'[)') ) {
 else if ( moment(thisHour).isBetween(17,19,null,'[)') ) {
   console.log(vows['06'])
 }
+=======
+const rotation = require('./rotation')
+const sessions = [
+  {
+    startTime: 6,
+    endTime: 8
+  },
+  {
+    startTime: 8,
+    endTime: 10
+  },
+  {
+    startTime: 10,
+    endTime: 12
+  },
+  {
+    startTime: 12,
+    endTime: 15
+  },
+  {
+    startTime: 15,
+    endTime: 17
+  },
+  {
+    startTime: 17,
+    endTime: 19
+  }
+]
+
+const moment = require('moment')
+const thisHour = moment().hour()
+for (session of sessions) {
+  if (moment(thisHour).isBetween(session.startTime, session.endTime, null, '[)')) {
+    console.log(rotation.currentVow)
+  }
+}
+
+// Days have sessions
+// sessions have startTimes, endTimes, and vows
+// session needs to know what the last vow was or be told what it's current vow is.
+>>>>>>> c4aef7215494d8041fca50983609dea11352c3ac
